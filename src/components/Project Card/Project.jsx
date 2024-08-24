2cfc35eb0c2bb688e85d9c6aeceabf951b53d434
LEONARDO FIGUEIREDO DO NASCIMENTO <kriptonascimento@hotmail.com>
2024-08-24 07:30:38 -0300

    Deploy 1.0.1

--- src/components/Project Card/Project.jsx
+++ src/components/Project Card/Project.jsx
@@ -8,13 +8,13 @@ function Project(props){
             <h4>{props.description}</h4>
             <div className='tech-stack'>
                 {props.techStack.map((tech, index)=>(
-                    <img className="tech-used" src={`/languages and lkills/${tech}`}/>
+                    <img className="tech-used" src={`/Languages and Skills/${tech}`}/>
                 ))}
             </div>
 
             <div className="project-links">
-                {props.video ? <a href={props.video} target="_blank" className='yt-video'><img src="/photos/youtube-icon.png"/>Video</a> : <></>}
-                {props.repository ? <a href={props.repository} target="_blank" className='git-repo'><img src="/photos/github-icon2.png"/> Repository</a> : <></>}
+                {props.video ? <a href={props.video} target="_blank" className='yt-video'><img src="/Photos/youtube-icon.png"/>Video</a> : <></>}
+                {props.repository ? <a href={props.repository} target="_blank" className='git-repo'><img src="/Photos/github-icon2.png"/> Repository</a> : <></>}
             </div>
             
         </div>
--- src/components/Skill Card/SkillCard.jsx
+++ src/components/Skill Card/SkillCard.jsx
@@ -3,7 +3,7 @@ import './SkillCard.css'
 function SkillCard(props){
     return (
         <div className="skill-div">
-            <img src={`/languages and skills/${props.img}`}/>
+            <img src={`/Languages and Skills/${props.img}`}/>
             <h3>{props.name}</h3>
             {props.level ? <label>Level:</label> : <></>}
             <h4>{props.level}</h4>
